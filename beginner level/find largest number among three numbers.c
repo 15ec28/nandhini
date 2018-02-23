8547e0c332aa29bdbfaa9f14e29efa749bea29ca
15ec28 <nandhiniengg95@gmail.com>
2018-02-23 09:43:24 +0530

    Create find largest number among three numbers.c

---
+++ beginner level/find largest number among three numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main()
+{
+int a,b, c;
+printf("Enter the character");
+scanf("%d %d %d",&a,&b,&c);
+if(a>=b && a>=c) 
+{
+printf("%d is a largest number",a);
+}
+if(b>=a && b>=c) 
+{
+printf("%d is a largest number",b);
+}
+if(c>=a && c>=b) 
+{
+printf("%d is a largest number",c);
+}
+return 0;
+}
